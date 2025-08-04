703e7834a20c734c918f760534a73fefeee4f500
Adam Worede <adamg2609@gmail.com>
2025-08-04 15:39:14 +0100

    pyspark databricks aws connection

--- pyspark_streaming_job/streaming_job.py
+++ pyspark_streaming_job/streaming_job.py
@@ -1,16 +1,21 @@
-import sys, os, time
+import sys
+import os
+import time
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, to_timestamp, current_timestamp
-from pyspark.sql.types import StructType, StringType, FloatType
+from pyspark.sql.functions import (
+    from_json,
+    col,
+    to_timestamp,
+    current_timestamp
+)
+from pyspark.sql.types import (
+    StructType,
+    StringType,
+    FloatType
+)
 from dotenv import load_dotenv
 
 load_dotenv(os.path.join(os.path.dirname(__file__), '../.env'))
-
-root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if root not in sys.path:
-    sys.path.insert(0, root)
-
-load_dotenv(os.path.join(root, ".env"))
 
 from redshift_connection.connection import redshift_connection_params
 
@@ -18,18 +23,16 @@ from redshift_connection.connection import redshift_connection_params
 spark = SparkSession.builder \
     .appName("StockDataStreamProcessor") \
     .config("spark.jars.packages",
-        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,"
-        "com.amazon.redshift:redshift-jdbc42:2.1.0.12,"
-        "org.apache.spark:spark-avro_2.12:3.5.1") \
-    .config("spark.driver.memory", "2g") \
-    .config("spark.executor.memory", "2g") \
-    .config("spark.driver.maxResultSize", "1g") \
-    .config("spark.sql.adaptive.enabled", "false") \
+            ",".join([
+                "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1",
+                "com.amazonaws:aws-java-sdk-bundle:1.12.430",
+                "com.databricks:spark-redshift_2.12:4.0.2"
+            ])) \
     .getOrCreate()
 
 spark.sparkContext.setLogLevel("WARN")
 
-# loggin the version to the console for verification 
+# logging the version to the console for verification 
 print(f"Spark session created successfully")
 print(f"Spark version: {spark.version}")
 print(f"Java version: {spark.sparkContext._jvm.System.getProperty('java.version')}")
@@ -49,50 +52,67 @@ schema = StructType() \
     .add("volume", StringType())
 
 # read from Kafka messages
-df = spark.readStream \
-    .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:29092") \
-    .option("subscribe", "stock_data") \
-    .option("startingOffsets", "earliest") \
-    .option("failOnDataLoss", "false") \
-    .load() \
-  .select(from_json(col("value").cast("string"), schema).alias("d")) \
-  .select("d.*") \
-  .withColumn("datetime", to_timestamp("datetime","yyyy-MM-dd HH:mm:ss")) \
-  .withColumn("open",  col("open").cast(FloatType())) \
-  .withColumn("high",  col("high").cast(FloatType())) \
-  .withColumn("low",   col("low").cast(FloatType())) \
-  .withColumn("close", col("close").cast(FloatType())) \
-  .withColumn("volume",col("volume").cast(FloatType()))
+df = (
+    spark.readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "localhost:29092") \
+        .option("subscribe", "stock_data") \
+        .option("startingOffsets", "earliest") \
+        .option("failOnDataLoss", "false") \
+        .load() \
+        .select(from_json(col("value").cast("string"), schema).alias("d")) \
+        .select("d.*") \
+        .withColumn("datetime_timestamp", to_timestamp("datetime","yyyy-MM-dd HH:mm:ss")) \
+        .withColumn("open_price",  col("open").cast(FloatType())) \
+        .withColumn("high",  col("high").cast(FloatType())) \
+        .withColumn("low",   col("low").cast(FloatType())) \
+        .withColumn("close", col("close").cast(FloatType())) \
+        .withColumn("volume",col("volume").cast(FloatType())) \
+        .select(
+            "ticker",
+            "interval",
+            "currency",
+            "exchange_timezone",
+            "exchange",
+            "date_timestamp",
+            "open_price",
+            "high",
+            "low",
+            "close",
+            "volume"
+        )
+)
 
 # write micro-batches to Snowflake
-def write_redshift(batch_df, batch_id):
-    try:
-        if batch_df.count() > 0:
+def write_to_redshift(batch_df, batch_id):
+    if batch_df.rdd.isEmpty():
+        print(f"Batch {batch_id} has no new rows")
+        return
+    else:
+        batch_df = batch_df.withColum("processing_time", current_timestamp())
 
-            batch_df = batch_df.withColumn("processing_time", current_timestamp())
-
-            print(f"Processing batch {batch_id} with {batch_df.count()} records...")
+        # spark-databricks redshift connector, stages the PySpark dataframe to S3 and runs copy
+        (
             batch_df.write \
-                .format("jdbc") \
-                .options(**redshift_connection_params) \
-                .option("dbtable", os.getenv("REDSHIFT_TABLE")) \
+                .format("com.databricks.spark-redshift") \
+                .option("url", redshift_connection_params["url"]) \
+                .option("dtable", redshift_connection_params["temp_s3_directory"]) \
+                .option("tempdir", redshift_connection_params["temp_s3_directory"]) \
+                .option("aws_iam_role", redshift_connection_params["aws_iam_role"]) \
                 .mode("append") \
                 .save()
-            print(f"Batch {batch_id}: Successfully written {batch_df.count()} records added to Redshift.")
-        else:
-            print(f"Batch {batch_id}: No data to process")
-    except Exception as e:
-        # can also add failed batches to S3
-        print(f"Batch {batch_id} error: {e}")
-
+        )
+        count = batch_df.count()
+        print(f"Batch {batch_id} successfully wrote {count} rows into {redshift_connection_params["redshift_table"]}")
 # start streaming query
 print("Starting streaming query...")
-query = df.writeStream \
-    .trigger(processingTime="10 seconds") \
-    .foreachBatch(write_redshift) \
-    .option("checkpointLocation", "/tmp/checkpoints/redshift_stock_data") \
-    .start()
+query = (
+    df.writeStream \
+        .trigger(processingTime="10 seconds") \
+        .foreachBatch(write_to_redshift) \
+        .option("checkpointLocation", "/tmp/checkpoints/redshift_stream")
+        .start()
+)
 
 try:
     print("Streaming job is running.")
--- redshift_connection/connection.py
+++ redshift_connection/connection.py
@@ -9,5 +9,8 @@ redshift_connection_params = {
     "url": os.getenv("REDSHIFT_JDBC_URL"),
     "user": os.getenv("REDSHIFT_USER"),
     "password": os.getenv("REDSHIFT_PASSWORD"),
-    "driver": "com.amazon.redshift.jdbc42.Driver"
+    "driver": "com.amazon.redshift.jdbc42.Driver",
+    "temp_s3_directory": os.getenv("TEMP_S3_DIRECTORY"),
+    "aws_iam_role": os.getenv("AWS_IAM_ROLE"),
+    "redshift_table": os.getenv("REDSHIFT_TABLE")
 }
